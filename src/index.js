dd76010fec87e1b7fe1abe8b90044e30d92f6e39
Alessandro <sandroledesma@gmail.com>
2024-05-02 12:32:35 -0400

    add new files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // index.js
 
-document.addEventListener("DOMContentLoaded", (event) => { // ensures that the DOM is fully loaded before executing the code
   function displayRamen() { 
     fetch("http://localhost:3000/ramen") // fetch the images from the db.json database
     .then((response) => response.json()) // converts the fetched response into JSON format
@@ -24,73 +23,71 @@ document.addEventListener("DOMContentLoaded", (event) => { // ensures that the D
         const detailImage = document.querySelector('.detail-image'); 
         const nameOfRamen = document.querySelector('.name');
         const restaurantOfRamen = document.querySelector('.restaurant');
-        // why are rating and comment different from above? 
-        const ratingOfRamen = document.getElementById('rating-display'); 
-        const commentOfRamen = document.getElementById('comment-display');
+        const ratingOfRamen = document.querySelector('#rating-display'); 
+        const commentOfRamen = document.querySelector('#comment-display');
 
       detailImage.src = ramen.image;
-      detailImage.slt = ramen.image;
+      detailImage.alt = ramen.image;
       nameOfRamen.textContent = ramen.name;
       restaurantOfRamen.textContent = ramen.restaurant;
       ratingOfRamen.textContent = ramen.rating;
       commentOfRamen.textContent = ramen.comment;
     }
-    window.onload = displayRamen;
   
-    // function addSubmitListener(newRamen) {
-    //   const addNewRamen = document.getElementById("new-ramen")
-    //   addNewRamen.addEventListener("submit", event => {
-    //     event.preventDefault();
+    function addSubmitListener() {
+      const addNewRamen = document.getElementById("new-ramen");
+      addNewRamen.addEventListener("submit", event => {
+        event.preventDefault();
 
-    //     const newName = event.target["new-name"].value;
-    //     const newRestaurant = event.target["new-restaurant"].value;
-    //     const newImage = event.target["new-image"].value;
-    //     const newRating = event.target["new-rating"].value;
-    //     const newComment = event.target["new-rating"].value;
+        const newName = document.getElementById("new-name").value;
+        const newRestaurant = document.getElementById("new-restaurant").value;
+        const newImage = document.getElementById("new-image").value;
+        const newRating = document.getElementById("new-rating").value;
+        const newComment = document.getElementById("new-comment").value;
 
-    //     fetch("http://localhost:3000/ramen")
-    //       method: "POST", 
-    //       headers: {
-    //           "Content-Type": "application/json",
-    //           "Accept": "application/json",            
-    //       },
-    //       body: JSON.stringify({ // fields/key below should be identical to db.json
-    //         name: newName, 
-    //         restaurant: newRestaurant, 
-    //         image: newImage,
-    //         rating: newRating,
-    //         comment: newComment
-    //       })
-    //     .then((response) => response.json())
-    //     .then(newRamen => { // this .then element is to place image on the ramen carousel?
-    //         const imageOfRamen = document.getElementById("ramen-menu"); // "connects" the image pulled into the HTML id="ramen-menu"
-    //         ramen.forEach(ramen => { // loop through each ramen object in the database ("for each object, take image and name")
-    //           const img = document.createElement('img'); // create an image element for each ramen image
-    //           img.src = ramen.image; // pulls the image src url from db.json (key: image)
-    //           img.alt = ramen.name; // pulls the image name from db.json (key: name)
-    //           imageOfRamen.appendChild(img); 
-    //         })
-    //     })
-    //   } 
-      
+        fetch("http://localhost:3000/ramen", { //post request should end with {} for the code block of method and headers
+          method: "POST", 
+          headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json",            
+          },
+          body: JSON.stringify({ // new fields and key 
+            name: newName, 
+            restaurant: newRestaurant, 
+            image: newImage,
+            rating: newRating,
+            comment: newComment
+          })
+        })
+        .then((response) => response.json())
+        .then(data => { // why are images getting doubled up at refresh
+            console.log("New Ramen added:", data);
+            // displayRamen();
+            //newImage.appendChild(img);
+            clearForm();
+        })
+        .catch(error => console.error('Error adding new ramen:', data));
     });
-  
+  }
+
+  function clearForm() {
+    document.getElementById("new-name").value = "";
+    document.getElementById("new-restaurant").value = "";
+    document.getElementById("new-image").value = "";
+    document.getElementById("new-rating").value = "";
+    document.getElementById("new-comment").value = "";
+  }
+
+  function main() {
+    displayRamen();
+    addSubmitListener();
+  }
+
+  main();
 
 
 
-  
-  /*
-
-
-const main = () => {}
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
-
-
-
-})
-
+/*
 
 // Export functions for testing
 export {
